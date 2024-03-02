48d33f33ba9db7010567bf8c4fe57643467b5068
Movash <andrushhenko97@gmail.com>
2024-03-02 21:48:58 +0200

    remove comment

--- routes/api/drugstores.js
+++ routes/api/drugstores.js
@@ -2,14 +2,6 @@ const express = require("express");
 
 const ctrl = require("../../controllers/drugstores");
 
-// const {
-//   validateBody,
-//   validateFavorite,
-//   isValidId,
-// } = require("../../middlewares");
-
-// const { schemas } = require("../../models/drugstore");
-
 const router = express.Router();
 
 router.get("/", ctrl.allDrugstores);
