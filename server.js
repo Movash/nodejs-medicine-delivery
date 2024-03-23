7145af61a2e97e0c8270792c976c2554d2c76df6
Movash <andrushhenko97@gmail.com>
2024-03-23 19:22:20 +0200

    add_make_request

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const mongoose = require("mongoose")
+const {makeRequest} = require("./utils/cronjob");
 
 const app = require('./app')
 
 const {DB_HOST, PORT = 3000} = process.env
 
-  mongoose.connect(DB_HOST)
+mongoose
+  .connect(DB_HOST)
   .then(() => {
-    app.listen(PORT);
-    console.log("Database connection successful")
+    console.log("Database connection successful");
+
+    const server = app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+
+      makeRequest();
+    });
+
+    server.on("error", (error) => {
+      console.error("Error occurred:", error);
+    });
   })
   .catch((error) => {
     console.log(error.message);
-    process.exit(1)
-  })
+    process.exit(1);
+  });
